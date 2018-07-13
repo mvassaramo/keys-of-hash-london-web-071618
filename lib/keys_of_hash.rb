b5a7d44d5c03992562b464d2b43cd9bde57d0e51
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-13 19:53:59 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,21 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  def keys_of(*arguments)
+values_array=[] 
+
+  self.each do |key, value| 
+      if arguments.include?(value)
+        values_array << key
+      end
+    end
+    values_array
+  end 
 end
+
+  # arguments.each do |x|
+  #   if self.has_value?(x)
+  #     binding.pry
+  #     values_array << x
+  #   end 
+  # end
